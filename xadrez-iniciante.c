2e5b91789a7f83d98a483830cd4b941f2211aac3
Pelicaio <162237013+Pelicaio@users.noreply.github.com>
2025-05-13 20:02:24 +0000

    Nivel aventureiro concluido

--- xadrez-iniciante.c
+++ xadrez-iniciante.c
@@ -1,12 +1,31 @@
-int main(){
-    //Movimento da Torre utilizando o FOR
+#include <stdio.h>
 
-    for(int = 1, i < 5, i++){
-        printf("Direita/n");
+
+int main(){
+    //Movimento da Rainha com DO WHILE
+    int i = 0;
+    do{
+        printf("Esquerda\n", i);
+        i++;
+    } while(i <= 7);
+
+   
+    //Movimento da Torre com FOR
+    for(int z = 0; z < 5; z++){
+        printf("Direita\n");
+    }
+  
+   
+    //Movimento do Bispo com WHILE
+    int x = 0;
+    while(x < 5){
+        printf("Cima,Direita\n");
+        x++;
+
     }
     return 0;
 
+    
 
-
-
+    
 }
